459666a01aa537363929b51d5fb2f7f825df6b30
Lukas <lukasstrassel@googlemail.com>
2023-01-25 17:31:50 +0100

    fix: diff impls

--- diff.js
+++ diff.js
@@ -2,6 +2,7 @@
 require("dotenv").config();
 const child_process = require("child_process");
 const fs = require("fs");
+const { utils } = require("ethers");
 const {
   AaveV3Ethereum,
   AaveV3Polygon,
@@ -30,26 +31,57 @@ function download(chain, name, address) {
   }
 }
 
+function getImpl(chain, address) {
+  return utils.hexStripZeros(
+    runCmd(
+      `cast storage --rpc-url ${chain} ${address} 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc`
+    ).replace("\n", ""),
+    32
+  );
+}
+
 const INVALID_KEYS = [
-  "CHAIN_ID",
-  "REPAY_WITH_COLLATERAL_ADAPTER",
-  "SWAP_COLLATERAL_ADAPTER",
+  "CHAIN_ID", // random artifact
+  "REPAY_WITH_COLLATERAL_ADAPTER", // utility
+  "SWAP_COLLATERAL_ADAPTER", // utility
+  "LISTING_ENGINE", // utility
+  "ACL_ADMIN", // chain specific
+];
+
+const PROXIES = [
+  "COLLECTOR",
+  "DEFAULT_INCENTIVES_CONTROLLER",
+  "POOL",
+  "POOL_CONFIGURATOR",
 ];
 
 function downloadContracts(networkName, config) {
   Object.keys(config)
     .filter((key) => !INVALID_KEYS.includes(key))
-    .map((key) => download(networkName, key, config[key]));
+    .map((key) => {
+      const isProxy = PROXIES.includes(key);
+      download(
+        networkName,
+        isProxy ? `${key}_IMPL` : key,
+        isProxy ? getImpl(networkName, config[key]) : config[key]
+      );
+    });
 }
 
 function diffContracts(chain, config) {
   Object.keys(config)
     .filter((key) => !INVALID_KEYS.includes(key))
-    .map((key) =>
+    .map((key) => {
+      const isProxy = PROXIES.includes(key);
+
       runCmd(
-        `make git-diff before=downloads/${chain}/${key} after=downloads/mainnet/${key} out=${chain}_${key}`
-      )
-    );
+        `make git-diff before=downloads/${chain}/${
+          isProxy ? `${key}_IMPL` : key
+        } after=downloads/mainnet/${
+          isProxy ? `${key}_IMPL` : key
+        } out=${chain}_${isProxy ? `${key}_IMPL` : key}`
+      );
+    });
 }
 
 async function main() {
