8be84016a81cd8c1f005ba699a27a4c07c34194a
eboado <ebdmrr@gmail.com>
2023-05-01 09:25:30 +0200

    Update CreateProposal script with new deployed payloads

--- scripts/CreateProposal.s.sol
+++ scripts/CreateProposal.s.sol
@@ -7,10 +7,10 @@ import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
 contract CreateUpgradeProposal is Script {
   function run() external {
     GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](4);
-    payloads[0] = GovHelpers.buildMainnet(0x31A239f3e39c5D8BA6B201bA81ed584492Ae960F); // https://etherscan.io/address/0x31a239f3e39c5d8ba6b201ba81ed584492ae960f
-    payloads[1] = GovHelpers.buildPolygon(0xa603Ad2b0258bDda94F3dfDb26859ef205AE9244); // https://polygonscan.com/address/0xa603ad2b0258bdda94f3dfdb26859ef205ae9244
-    payloads[2] = GovHelpers.buildOptimism(0x7748d38A160EEeF9559e2b043eAec5CfFFCE3E4c); // https://optimistic.etherscan.io/address/0x7748d38a160eeef9559e2b043eaec5cfffce3e4c
-    payloads[3] = GovHelpers.buildArbitrum(0x209Ad99bd808221293d03827B86cC544bcA0023b); // https://arbiscan.io/address/0x209ad99bd808221293d03827b86cc544bca0023b
+    payloads[0] = GovHelpers.buildMainnet(0x6F6caee8348c10dC9441Fbe28cb588566F1251D5); // https://etherscan.io/address/0x6f6caee8348c10dc9441fbe28cb588566f1251d5
+    payloads[1] = GovHelpers.buildPolygon(0xb56c0688316c333bFAA20cD7d836e05d48939b58); // https://polygonscan.com/address/0xb56c0688316c333bfaa20cd7d836e05d48939b58
+    payloads[2] = GovHelpers.buildOptimism(0x45CE944c29A25AEE0e135f8f89aB55Dc9c5438E5); // https://optimistic.etherscan.io/address/0x45ce944c29a25aee0e135f8f89ab55dc9c5438e5
+    payloads[3] = GovHelpers.buildArbitrum(0xF876Fa9A96EcD51c0BBa8554Ff2e397fE7F73BAB); // https://arbiscan.io/address/0xf876fa9a96ecd51c0bba8554ff2e397fe7f73bab 
 
     vm.startBroadcast();
     GovHelpers.createProposal(
